bd1984c79d8669804d8845cb223b88d733be7254
Chris Manson <chris@manson.ie>
2023-09-07 14:45:54 +0100

    add notes

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 import Application from '@ember/application';
-import Resolver from 'ember-resolver';
+import Resolver from '@embroider/resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'vite-app/config/environment';
+
+import resolverModules from '#embroider_compat/app';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(resolverModules);
 }
 
 loadInitializers(App, config.modulePrefix);

281a3204ccac1afc1772cbffa9c55b2fe1266038
Chris Manson <chris@manson.ie>
2023-12-14 17:13:27 +0000

    make sure dependency discovery finds the template-only component

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'vite-app/config/environment';
 
+// TODO find out why this isn't being found properly
+import "@ember/component/template-only.js"
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
